2d14d9561eabb2929543f5e29274457e038f9754
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-09-08 17:11:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+const userName = prompt('Type your name: ');
+
+console.log(`the tiped name was:  ${userName}`);
+
+const peso = prompt('Escreva seu peso: ');
+
+console.log(`Seu Peso é:  ${peso}`);
+
+const altura = prompt('Escreva sua altura: ');
+
+console.log(`Sua altura é:  ${altura}`);
+
+const diabetico = prompt('você é diabetico? sim ou não ');
+
+console.log(`você é diabetico :  ${diabetico}`);
---
+++ u.js
@@ -0,0 +1,7 @@
+
+
+const peso = ('escreva o peso: ')();
+
+const nome = ('escreva o nome: ')();
+
+const altura =('escreva a altura: ')();
